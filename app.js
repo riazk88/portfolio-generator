1985d1e31421c3100083d7221524c2b19bac0531
Kiran88 <62086331+riazk88@users.noreply.github.com>
2020-05-14 10:33:59 -0600

    inquirer

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
 
-const fs = require('fs');
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
 
-const profileDataArgs = process.argv.slice(2);
+// const generatePage = require('./src/page-template.js');
 
-const [name, github] = profileDataArgs;
+// const fs = require('fs');
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-  if (err) throw new Error(err);
+// const profileDataArgs = process.argv.slice(2);
 
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+// const [name, github] = profileDataArgs;
+
+// fs.writeFile('./index.html', generatePage(name, github), err => {
+//   if (err) throw new Error(err);
+
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
 
 
